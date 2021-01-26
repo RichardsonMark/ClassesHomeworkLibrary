9fef4c202546bad927e19d8bbc7b85897e1c2033
Mark Richardson <mark.richardson4@gmail.com>
2021-01-26 17:09:05 +0000

    first test written checking a book 'hasTitle' and 'getTitle' method written to pass test

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,2 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class BookTest {
+
+    Book book;
+
+    @Before
+    public void before() {
+        book = new Book("Ubik", "Philip K Dick", "Science Fiction");
+    }
+
+    @Test
+    public void hasTitle() {
+        assertEquals("Ubik", book.getTitle());
+    }
 }
